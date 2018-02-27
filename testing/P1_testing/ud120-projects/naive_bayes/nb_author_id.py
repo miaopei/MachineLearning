d9591168f61707f0ee9cfebc0d78cdc9bd5722ae
miaopei <miaopei163@163.com>
2018-02-27 23:37:23 +0800

    commit p1 test code

--- testing/P1_testing/ud120-projects/naive_bayes/nb_author_id.py
+++ testing/P1_testing/ud120-projects/naive_bayes/nb_author_id.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+# -*- coding: utf-8 -*-
 """ 
     This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
 
@@ -9,19 +9,17 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
 
 
 #########################################################
@@ -30,6 +28,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
+clf = GaussianNB()
 
+t0 = time()
 
+clf.fit(features_train, labels_train)
+
+print "training time: ", round(time() - t0, 3), "s"
+
+t0 = time()
+
+pred = clf.predict(features_test)
+
+print "predict time: ", round(time() - t0, 3), "s"
+
+# accuracy = clf.score(features_test, labels_test) # 得到的 accuracy 结果一样
+accuracy = accuracy_score(pred, labels_test)
+
+print "accuracy: ", accuracy
--- testing/P1_testing/ud120-projects/svm/svm_author_id.py
+++ testing/P1_testing/ud120-projects/svm/svm_author_id.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+# -*- coding: utf-8 -*-
 """ 
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
@@ -27,4 +27,34 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+
+clf = svm.SVC(kernel='rbf', C=10000.)
+
+t0 = time()
+
+clf.fit(features_train, labels_train)
+
+print "training time: ", round(time() - t0, 3), "s"
+
+t0 = time()
+
+pred = clf.predict(features_test)
+
+print "predict time: ", round(time() - t0, 3), "s"
+
+# accuracy = clf.score(features_test, labels_test) # 得到的 accuracy 结果一样
+accuracy = accuracy_score(pred, labels_test)
+
+print "accuracy: ", accuracy
+
+print "10: ", pred[10]
+print "26: ", pred[26]
+print "50: ", pred[50]
+
+from collections import Counter
+print "svm(Chris(1), Sara(0)): ", Counter(pred)
